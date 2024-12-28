22b7c043ca8c3ca92910b828ce7dd87579a68a74
Rim9712 <rimlax9@gmail.com>
2024-12-28 13:28:24 +0100

    Changes as of 12/28

--- momentum_strategy/src/startegy.py
+++ momentum_strategy/src/startegy.py
@@ -2,23 +2,28 @@ import pandas as pd
 import yfinance as yf
 import pandas_ta as ta
 
-def get_stock_data(symbol, start_date, end_date) -> pd.DataFrame: 
+def get_stock_data(tickers:list, start_date, end_date) -> pd.DataFrame: 
         """
         Get stock historical data 
         """
-        try:
-            # Ensuring dates are in string format compatible with yfinance
-            start_date_e = pd.to_datetime(start_date, format = '%Y/%m/%d')
-            end_date_e = pd.to_datetime(end_date, format = '%Y/%m/%d')
-            
-            # Fetch data using adjusted dates
-            data = yf.download(symbol,start = start_date_e, end = end_date_e)
-            return data
-        
-        except Exception as e:
-            print(f"Error downloading{symbol}:{e}")
-            return None
+        # Ensuring dates are in string format compatible with yfinance
+        start_date_e = pd.to_datetime(start_date, format = '%Y/%m/%d')
+        end_date_e = pd.to_datetime(end_date, format = '%Y/%m/%d')
 
+        # Fetch data using adjusted dates
+        data = []
+        for ticker in tickers: 
+            try:
+                df= yf.download(ticker,start = start_date_e, end = end_date_e)
+                df.columns = [f'{col}' for col in df.columns]
+                data.append(df)
+                print(f"Succesfully downloaded {ticker}")
+            except Exception as e:
+                print(f"Error downloading {ticker}:{e}")
+                return None
+        combined_df = pd.concat(data, axis=1)
+        return combined_df
+        
 def save_data(data: pd.DataFrame, filename: str):
         """
         Save data to csv 
@@ -26,7 +31,7 @@ def save_data(data: pd.DataFrame, filename: str):
         data.to_csv(f"{filename}.csv")
 
 
-df = get_stock_data('AAPL', "2020/01/01","2024/01/01")
+df = get_stock_data(['AAPL', 'MSFT', 'GOOGL'], "2020/01/01","2024/01/01")
 save_data(df, "Apple data")
 
 
